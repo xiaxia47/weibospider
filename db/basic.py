f96730103a85c04e490b75a61e712d6db6d4c654
Sheldon_xia <18192368+xiaxia47@users.noreply.github.com>
2017-11-08 10:40:12 +0800

    增加注释

--- db/basic.py
+++ db/basic.py
@@ -18,10 +18,12 @@ def get_engine():
 
 
 eng = get_engine()
+#declarative_base类维持了一个从类到表的关系，通常一个应用使用一个base实例，所有实体类都应该继承此类对象
+#http://docs.sqlalchemy.org/en/latest/orm/extensions/declarative/api.html?highlight=declarative_base#sqlalchemy.ext.declarative.declarative_base
 Base = declarative_base()
-Session = sessionmaker(bind=eng)
 #创建一个mysql的session,管理和维护所有和mysql相关的数据库操作
 #http://docs.sqlalchemy.org/en/latest/orm/session_api.html?highlight=sessionmaker#sqlalchemy.orm.session.sessionmaker
+Session = sessionmaker(bind=eng)
 db_session = Session()
 #创建MetaData对象，用来对表进行管理，如建新表，或者删除不需要的表。此处主要为创建缺失数据表
 #http://docs.sqlalchemy.org/en/latest/orm/extensions/declarative/basic_use.html?highlight=metadata#accessing-the-metadata
